ebaf1ec2368e7449ce21322737f6a5e203242222
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-08-19 16:17:12 -0300

    Criado Schema, Model, novo documento de exemplo e salvado no mongoDB

--- app.js
+++ app.js
@@ -3,12 +3,32 @@ const app = express();
 const port = 3005;
 const mongoose = require("mongoose");
 
+const linkSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    url: String,
+    click: Number,
+});
+
+const Link = mongoose.model("Link", linkSchema);
+
+let link = new Link({
+    title: "Pedro",
+    description: "Link para o instagram",
+    url: "https://www.instagram.com/",
+    click: 0,
+});
+
+link.save()
+    .then((doc) => console.log(doc))
+    .catch((err) => console.log(err));
+
 mongoose.connect("mongodb://localhost:27017/links", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-const db = mongoose.connection;
+let db = mongoose.connection;
 db.on("error", () => console.log("Houve um erro"));
 db.once("open", () => console.log("Banco carregado"));
 
