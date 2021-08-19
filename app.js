34edbf4946a3397f0f14285850e89c706a402898
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-08-19 18:20:59 -0300

    criado requisição get para documentos do db e redirecionamento para url definida no documento

--- app.js
+++ app.js
@@ -3,34 +3,37 @@ const app = express();
 const port = 3005;
 const mongoose = require("mongoose");
 
-const linkSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    url: String,
-    click: Number,
-});
-
-const Link = mongoose.model("Link", linkSchema);
-
-let link = new Link({
-    title: "Pedro",
-    description: "Link para o instagram",
-    url: "https://www.instagram.com/",
-    click: 0,
-});
-
-link.save()
-    .then((doc) => console.log(doc))
-    .catch((err) => console.log(err));
-
 mongoose.connect("mongodb://localhost:27017/links", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+const linkSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: String,
+    url: { type: String, required: true },
+    click: { type: Number, default: 0 },
+});
+
+const Link = mongoose.model("Link", linkSchema);
+
 let db = mongoose.connection;
 db.on("error", () => console.log("Houve um erro"));
-db.once("open", () => console.log("Banco carregado"));
+db.once("open", () => {
+    console.log("Banco carregado");
+
+    app.get("/:title", async (req, res) => {
+        let title = req.params.title;
+
+        try {
+            let doc = await Link.findOne({ title });
+            console.log(doc);
+            res.redirect(doc.url);
+        } catch (err) {
+            console.log(err);
+        }
+    });
+});
 
 app.get("/", (req, res) => res.send("Hello friend!"));
 
