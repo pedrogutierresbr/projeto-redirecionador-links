3484e289a53fc1533671989e44bf0a70d2e08c60
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-08-23 11:45:15 -0300

    criado no template para página de edição, criado rotas e e funcoes loadLink e editLink e implementado

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { redirect, addLink, allLinks, deleteLink } = require("../controllers/linkController");
+const {
+    redirect,
+    addLink,
+    allLinks,
+    deleteLink,
+    loadLink,
+    editLink,
+} = require("../controllers/linkController");
 
 router.get("/", allLinks);
 
@@ -9,7 +16,11 @@ router.get("/add", (req, res) => res.render("index", { error: false, body: {} })
 
 router.get("/:title", redirect);
 
+router.get("/edit/:id", loadLink);
+
 router.post("/", express.urlencoded({ extended: true }), addLink);
+
+router.post("/edit/:id", express.urlencoded({ extended: true }), editLink);
 
 router.delete("/:id", deleteLink);
 
