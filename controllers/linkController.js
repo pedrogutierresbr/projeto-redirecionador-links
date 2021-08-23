4bef29bda4fdd42cd6b5d3fafd78f818a6ab9ae7
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-08-23 17:35:31 -0300

    refatorado código

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -22,7 +22,7 @@ const addLink = async (req, res) => {
         await link.save();
         res.redirect("/");
     } catch (error) {
-        res.render("index", { error, body: req.body });
+        res.render("add", { error, body: req.body });
     }
 };
 
--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+var methodOverride = require("method-override");
 
 const {
     redirect,
@@ -10,9 +11,11 @@ const {
     editLink,
 } = require("../controllers/linkController");
 
+router.use(methodOverride("_method"));
+
 router.get("/", allLinks);
 
-router.get("/add", (req, res) => res.render("index", { error: false, body: {} }));
+router.get("/add", (req, res) => res.render("add", { error: false, body: {} }));
 
 router.get("/:title", redirect);
 
@@ -24,6 +27,6 @@ router.post("/edit/:id", express.urlencoded({ extended: true }), editLink);
 
 router.delete("/:id", deleteLink);
 
-router.delete("/", express.json(), deleteLink);
+router.delete("/", express.urlencoded({ extended: true }), deleteLink);
 
 module.exports = router;
